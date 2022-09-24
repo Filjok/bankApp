15ebf5bc3b2f5a60f5783678afcff084afb24b1b
Filjok <filjofijothundiyil@gmail.com>
2022-09-24 09:39:49 +0530

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  quote = "your perfect banking partner"
+
+  acno = ''
+  pass =''
+
+
+  userDetails:any = {
+    1000: { acno: 1000, username: 'amal', password: 123, balance: 1000000 },
+    1001: { acno: 1001, username: 'ajith', password: 123, balance: 100000 },
+    1002: { acno: 1002, username: 'subin', password: 123, balance: 1000 },
+    1003: { acno: 1003, username: 'ajo', password: 123, balance: 10000 }
+
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    var acno = this.acno
+    var pass = this.pass
+    let userDetails = this.userDetails
+
+    if(acno in userDetails){
+      if(pass == userDetails[acno]['password']){
+        alert("login successful")
+
+      }
+      else{
+        alert("incorrect password")
+      }
+
+    }
+    else{
+      alert("user dosen't exist")
+    }
+    
+  }
+
+  acnoChange(event: any) {
+    this.acno = event.target.value;
+
+  }
+  passChange(event:any){
+    this.pass= event.target.value
+    
+    
+    
+  }
+
+}
